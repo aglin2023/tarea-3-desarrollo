769c4a3ad68ee04e30b18e678a810747b263f5a4
Xaidev <neclxas3311@gmail.com>
2024-06-04 01:36:29 -0400

    Agrego clases BotonRellenar y BotonSacar para iniciar las acciones de los botones por separado y facilitar la lectura del codigo

---
+++ src/main/java/visual/BotonRellenar.java
@@ -0,0 +1,28 @@
+package visual;
+
+import logica.Expendedor;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BotonRellenar extends JButton {
+    private Expendedor expendedor;
+    private JPanelExpendedor panelExpendedor;
+
+    public BotonRellenar(Expendedor expendedor, JPanelExpendedor panelExpendedor) {
+        this.expendedor = expendedor;
+        this.panelExpendedor = panelExpendedor;
+        this.setOpaque(false);
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BotonRellenar.this.expendedor.rellenarDepositos();
+                BotonRellenar.this.panelExpendedor.cargarPanel();
+            }
+        });
+    }
+}
+
+
---
+++ src/main/java/visual/BotonSacar.java
@@ -0,0 +1,30 @@
+package visual;
+
+import logica.Deposito;
+import logica.Expendedor;
+import logica.Producto;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BotonSacar extends JButton {
+    private Expendedor expendedor;
+    private JPanelDepositoUnico panelDepositoUnico;
+
+    public BotonSacar(Expendedor expendedor, JPanelDepositoUnico panelDepositoUnico) {
+        this.expendedor = expendedor;
+        this.panelDepositoUnico = panelDepositoUnico;
+        this.setOpaque(false);
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BotonSacar.this.panelDepositoUnico.actualizarImagen();
+            }
+        });
+    }
+}
+
+
