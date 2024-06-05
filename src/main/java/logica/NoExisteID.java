cf24c0b8fc651d9623d02a43d392759fd4f652f4
Xaidev <neclxas3311@gmail.com>
2024-06-05 19:18:37 -0400

    Javadoc

--- src/main/java/logica/NoExisteID.java
+++ src/main/java/logica/NoExisteID.java
@@ -1,7 +1,16 @@
 package logica;
 
-public class NoExisteID extends Exception{
+/**
+ * Excepción lanzada cuando se intenta buscar un ID que no existe.
+ */
+public class NoExisteID extends Exception {
+    /**
+     * Constructor que toma un mensaje de error como parámetro.
+     *
+     * @param mensaje Mensaje de error que describe la excepción.
+     */
     public NoExisteID(String mensaje) {
         super(mensaje);
     }
 }
+
