e3ce15ea29bce517f718c477ba878feb8ac6ed76
Xaidev <neclxas3311@gmail.com>
2024-06-04 21:25:03 -0400

    Cambios en PanelPrincipal para utilizar gridBagLayout

--- src/main/java/visual/JPanelPrincipal.java
+++ src/main/java/visual/JPanelPrincipal.java
@@ -8,20 +8,57 @@ import java.awt.*;
 public class JPanelPrincipal extends JPanel {
     private JPanelExpendedor panelExpendedor;
     private JPanelDepositoUnico panelDepositoUnico;
+    private JPanelPrecios panelPrecios;
 
     public JPanelPrincipal(Expendedor expendedor){
-        this.setLayout(null);
-        this.setPreferredSize(new Dimension(1400,1000));
+//        this.setLayout(null);
+//        this.setPreferredSize(new Dimension(1400,1000));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        this.setLayout(layout);
 
         // Crea JPanelDepositoUnico y lo agrega a JPanelPrincipal
         panelDepositoUnico = new JPanelDepositoUnico(expendedor);
-        panelDepositoUnico.setBounds(0, 750, 700, 250);
-        this.add(panelDepositoUnico);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.ipadx = 700;
+        gbc.ipady = 230;
+//        gbc.fill = GridBagConstraints.VERTICAL;
+     //   gbc.anchor = GridBagConstraints.WEST;
+        this.add(panelDepositoUnico,gbc);
+
+
+//      panelDepositoUnico.setBounds(0, 750, 700, 250);
+//      this.add(panelDepositoUnico);
 
         // Crea JPanelExpendedor y lo agrega a JPanelPrincipal
         panelExpendedor = new JPanelExpendedor(expendedor, panelDepositoUnico);
-        panelExpendedor.setBounds(0, 0, 700, 750);
-        this.add(panelExpendedor);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.ipadx = 700;
+        gbc.ipady = 750;
+        gbc.fill = GridBagConstraints.NONE;
+     //   gbc.anchor = GridBagConstraints.WEST;
+        this.add(panelExpendedor,gbc);
+
+        panelPrecios = new JPanelPrecios();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.ipadx = 350;
+        gbc.ipady = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        this.add(panelPrecios, gbc);
+
+
+//      panelExpendedor.setBounds(0, 0, 700, 750);
+//      this.add(panelExpendedor);
 
 
     }
